925134dc985bb760ad053606c901e7b50c4199bd
nikov-afk <kallash.nniikov@gmail.com>
2026-07-06 16:27:17 -0300

    aula 19

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /*
-Aula 17
-    operador short para o tipo int
-    intervalo: -32.768 até 32.767
-    %hi ou %d
- */
+        Aula 19
+    operador long para o tipo int
+    %li / %ld
+*/
 
 int main(void) {
-    int y = 0;
-    short int x = 32767;
+    long long int y = 2147483647;
     printf("tamanho de um int na memoria: %d bytes\n", sizeof y);
-    printf("tamanho em memoria de um short int: %d\n", sizeof(x));
+    y++;
+    printf("tamanho em memoria de um  int: %lli\n", y);
     return 0;
 }
