97e5a2ce79e282a8ad701a8b27b8a37fd6b87dca
nikov-afk <kallash.nniikov@gmail.com>
2026-07-05 22:55:28 -0300

    aula 5

--- main.c
+++ main.c
@@ -6,12 +6,26 @@
  */
 
 int main(void) {
-
+/*
     int valor; // criei uma variavel para guardar um valor do tipo inteiro
     valor = 50;
     printf("Digite um valor inteiro: ");
     scanf("%d", &valor);
     printf("valor = %d\n", valor);
+
+    return 0;
+    */
+
+    //tipo nome
+    float numero = 3.1415;
+
+    printf("%.4f\n", numero);
+
+    float numero_ler;
+    printf("\nDigite um numero real: ");
+    scanf("%f", &numero_ler);
+
+    printf("\n Numero lido: %.2f", numero_ler);
 
     return 0;
 }
