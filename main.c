7f2d6fc4e7aa156bcce0f1305542b1ed289c0947
nikov-afk <kallash.nniikov@gmail.com>
2026-07-05 23:22:39 -0300

    aula 10

--- main.c
+++ main.c
@@ -1,44 +1,17 @@
 #include <stdio.h>
 
 /*
- * Ler caracteres funçao fgetc()
- *  aula 9
+ * Ler varios valores
+ *  aula 10
  */
 
 int main(void) {
-/*
-    int valor; // criei uma variavel para guardar um valor do tipo inteiro
-    valor = 50;
-    printf("Digite um valor inteiro: ");
-    scanf("%d", &valor);
-    printf("valor = %d\n", valor);
+    int numero_1, numero_2, numero_3;
 
-    return 0;
-    */
-
-    /*
-    //tipo nome
-    float numero = 3.1415;
-
-    printf("%.4f\n", numero);
-
-    float numero_ler;
-    printf("\nDigite um numero real: ");
-    scanf("%f", &numero_ler);
-
-    printf("\n Numero lido: %.2f", numero_ler);
-
-    return 0;
-    */
-
-    //tipo nome
-    char sexo;
-
-    printf("Digite seu sexo: (f, F. m ou M) ");
-    //scanf("%c", &sexo);
-    sexo = fgetc(stdin);
-    printf("Valor da variavel sexo: %c\n", sexo);
-
-
+    printf("Digite tres valores inteiros: ");
+    scanf("%d %d %d", &numero_1, &numero_2, &numero_3);
+    printf("O numero eh %d", numero_1);
+    printf("O numero eh %d", numero_2);
+    printf("O numero eh %d", numero_3);
     return 0;
 }
