4b3bb2ef0b7c59e6c6db1a6636681725db777178
nikov-afk <kallash.nniikov@gmail.com>
2026-07-05 21:43:49 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("\n------------------------------------------------\n\n");
+    printf("1 - logar 2 - cadastrar 3 - imprimir");
+    printf("\n----------------------------------------------\n\n");
+    printf("Valor retornado: %d",printf("Oi"));
+    return 0;
+}
