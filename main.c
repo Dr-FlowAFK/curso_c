2559c5b35fe84093fe8996b4191e624ea07bafcf
nikov-afk <kallash.nniikov@gmail.com>
2026-07-08 11:16:38 -0300

    aula 106

--- main.c
+++ main.c
@@ -4,14 +4,29 @@
 #include <time.h>
 
 /*
-    Aula 104: Como gerar números aleatórios dentro de um intervalo?
+    Aula 106: Como criar uma matriz?
 */
 
 int main(void) {
     int i;
-    int num2[10];
+    int num2[10]; // vetor
+    int matriz[3][3] = {}; //matriz
+    // tipo identificador
+    int vet[10];
+
+    int mat1[3][3] = {1,2,3,4,5,6,7,8,9};
+    /*
+            c0 c1 c2
+    linha 0  1  2  3
+    linha 1  4  5  6
+    linha 2  7  8  9
+    */
+
+    int mat2[][3] = {1,2,3,4,5,6,7};
+    int mat3[3][3] = {0};
     srand(time(NULL));
     for(i = 0; i < 10; i++) {
+
         //num2[i] = rand();// gera uma sequancia pre moldada melhor pra testes
         num2[i] = 1 + rand() % 100;
     }
