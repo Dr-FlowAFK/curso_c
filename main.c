67cfa8a24f69a172a63dc2d3bf0925039ba2006c
nikov-afk <kallash.nniikov@gmail.com>
2026-07-08 10:54:14 -0300

    aula 103

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <time.h>
 
 /*
-    Aula 100
-    Como imprimir o conteúdo de um vetor
+    Aula 103: Como gerar números aleatórios para preencher um vetor?
 */
 
 int main(void) {
-    int num[10];
-    int num2[] = {1,2,3,4,5};
-    int num3[5] = {1,2,3,4,5};
-    int num4[5] = {0};
-
-    char letras[100];
-    char vogais[5] = {'a','e','i','o','u'};
-
-    float notas[3] = {7.5, 8.3, 9.4};
-
-    printf("%d ",num2[1]);
+    int i;
+    int num2[10];
+    srand(time(NULL));
+    for(i = 0; i < 10; i++) {
+        //num2[i] = rand();// gera uma sequancia pre moldada melhor pra testes
+        num2[i] = rand();
+    }
+    printf("\n\n");
+    for(i = 0; i < 10; i++)
+        printf("%d ", num2[i]);
+    printf("\n\n");
 
     return 0;
 }
