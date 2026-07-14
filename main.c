f9e56abc63a218136fd20f261a5d1c47b8fdc1bc
nikov-afk <kallash.nniikov@gmail.com>
2026-07-14 09:17:36 -0300

    aula 147

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 /*
-    Aula 146: Criando uma função que retorna o tamanho de uma string em C
+    Aula 147:  Procedimento para imprimir uma string caracter por caracter 
 */
 
 int minha_strlem(char str[]) {
@@ -16,9 +16,18 @@ int minha_strlem(char str[]) {
     return tam;
 }
 
+void imprimir_string(char str[]) {
+    int i = 0;
+    while (str[i] != '\0') {
+        printf("%c", str[i++]);
+    }
+    printf("\n");
+}
+
 int main() {
     char vet[20] = {"Ola"};
     printf("%d\n", strlen(vet));
     printf("%d\n", minha_strlem(vet));
+    imprimir_string(vet);
     return 0;
 }
