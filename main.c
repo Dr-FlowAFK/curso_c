304a340c101d23edb6c36bb5aad3c2196cdb6300
nikov-afk <kallash.nniikov@gmail.com>
2026-07-16 09:31:58 -0300

    aula 167

--- main.c
+++ main.c
@@ -5,23 +5,32 @@
 #include <string.h>
 #include <math.h>
 /*
-    Aula 157:    Calcular o enésimo termo da sequência de FIBONACCI com recursão
+    Aula 167: Como criar tipos de dados em C com TYPEDEF STRUCT?
 */
-
-int fibonacci(int n) {
-    if (n == 1) {
-        return 0;
-    }else if (n == 2) {
-        return 1;
-    }else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-}
-
+typedef struct {
+    int idade;
+    char sexo;
+    char nome[30];
+}Pessoa;
+struct pessoa {
+    int idade;
+    char sexo;
+    char nome[30];
+};
 int main() {
-    int n;
-    printf("Digite um numero: ");
-    scanf("%d",&n);
-    printf("Fibonacci de numeros: %d\n",fibonacci(n));
+    Pessoa pessoa1;
+    struct pessoa pessoa2;
+
+    pessoa1.idade = 20;
+    pessoa1.sexo = 'M';
+    strcpy(pessoa1.nome, "Joh");
+
+    pessoa2.idade = 20;
+    pessoa2.sexo = 'M';
+    strcpy(pessoa2.nome, "Snow");
+
+    printf("Pessoa 1\n Nome: %s\n Idade: %d\n Sexo: %c\n",pessoa1.nome,pessoa1.idade,pessoa1.sexo);
+    printf("Pessoa 2\n Nome: %s\n Idade: %d\n Sexo: %c\n",pessoa2.nome,pessoa2.idade,pessoa2.sexo);
+
     return 0;
 }
