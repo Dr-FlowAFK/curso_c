ed69099a890681cc693455589eb08bbc1c0b4956
nikov-afk <kallash.nniikov@gmail.com>
2026-07-16 09:51:33 -0300

    aula 169

--- main.c
+++ main.c
@@ -5,9 +5,14 @@
 #include <string.h>
 #include <math.h>
 /*
-    Aula 168: Como preencher uma STRUCT com dados lidos do teclado?
+    Aula 169: Posso usar uma STRUCT dentro de outra STRUCT?
 */
+typedef struct nascimento {
+    int dia, mes, ano;
+}Nascimento;
+
 typedef struct pessoa{
+    Nascimento data_nascimento;
     int idade;
     char sexo;
     char nome[30];
@@ -19,20 +24,27 @@ int main() {
 
     printf("Digite seu nome: \n");
     fgets(pessoa1.nome,30,stdin);
-    
+
     printf("Digite sua idade: \n");
     scanf("%d",&pessoa1.idade);
     scanf("%c");
 
     printf("Digite seu sexo (f, f, M, m) : \n");
     scanf("%c",&pessoa1.sexo);
+    printf("Digite sua data de nascimento no formato (dd mm aaaa): \n");
+    scanf("%d%d%d",&pessoa1.data_nascimento.dia,&pessoa1.data_nascimento.mes,&pessoa1.data_nascimento.ano);
+
+
 
     pessoa2.idade = 20;
     pessoa2.sexo = 'M';
     strcpy(pessoa2.nome, "Snow");
 
     printf("Pessoa 1\n Nome: %s\n Idade: %d\n Sexo: %c\n",pessoa1.nome,pessoa1.idade,pessoa1.sexo);
+    printf(" Data de nascimento: %d/%d/%d \n",pessoa1.data_nascimento.dia,pessoa1.data_nascimento.mes,
+        pessoa1.data_nascimento.ano);
     printf("Pessoa 2\n Nome: %s\n Idade: %d\n Sexo: %c\n",pessoa2.nome,pessoa2.idade,pessoa2.sexo);
+
 
     return 0;
 }
